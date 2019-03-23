528b55f4bbea6d64d28a5237021e1178a15c5268
朱海峰 <zhuhaifeng@parkingwang.com>
2019-03-24 02:19:09 +0800

    add yaml config

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
@@ -8,6 +9,23 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// todo 通过cookies 来判断 v1 还是v2
 		w.Write([]byte("v7"))
+		// read cookie
+		var cookie, err = r.Cookie("env")
+		if err == nil {
+			var cookievalue = cookie.Value
+			io.WriteString(w, "<b>get cookie value is "+cookievalue+"</b>\n")
+			switch cookievalue {
+			case "prod":
+				w.Write([]byte("prod"))
+			case "test":
+				w.Write([]byte("test"))
+			case "stage":
+				w.Write([]byte("stage"))
+			default:
+				w.Write([]byte("cookievalue"))
+			}
+		}
+
 	})
 	http.ListenAndServe(":8801", nil)
 }

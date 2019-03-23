0fd6d09cedf5c5fd5fc1884f7fc7d26623985f23
朱海峰 <zhuhaifeng@parkingwang.com>
2019-03-24 03:42:33 +0800

    add yaml config

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("env:"))
 		hostname := os.Getenv("HOSTNAME")
 		w.Write([]byte(hostname))
+
 	})
 	http.ListenAndServe(":8801", nil)
 }

169b878ccd4566fcd4c40daa9446484489ec53b3
朱海峰 <zhuhaifeng@parkingwang.com>
2019-03-24 03:02:42 +0800

    add yaml config

--- main.go
+++ main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// read cookie
-		var cookie, err = r.Cookie("env")
-		if err == nil {
-			var cookievalue = cookie.Value
-			io.WriteString(w, "<b>get cookie value is "+cookievalue+"</b>\n")
-			switch cookievalue {
-			case "prod":
-				w.Write([]byte("prod"))
-			case "test":
-				w.Write([]byte("test"))
-			case "stage":
-				w.Write([]byte("stage"))
-			default:
-				w.Write([]byte("cookievalue"))
-			}
-		} else {
-			w.Write([]byte(err.Error()))
-		}
-
+		hostname := os.Getenv("HOSTNAME")
+		w.Write([]byte(hostname))
 	})
 	http.ListenAndServe(":8801", nil)
 }

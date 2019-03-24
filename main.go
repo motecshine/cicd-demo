6715e12ff143d7fdc7210a19c68dcc8b494b01bb
朱海峰 <zhuhaifeng@parkingwang.com>
2019-03-24 14:22:24 +0800

    add yaml config

--- main.go
+++ main.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("env:"))
+		w.Write([]byte("\n \r"))
 		hostname := os.Getenv("HOSTNAME")
-		w.Write([]byte(hostname))
-
+		w.Write([]byte("HOSTNAME:" + hostname + "\r \n"))
+		cookies, err := r.Cookie("env")
+		if err != nil {
+			// emm 没设置cookies 那就 读dev conf吧
+			config, err := ioutil.ReadFile("./configs/dev.json") // just pass the file name
+			if err != nil {
+				w.Write([]byte("\n \r"))
+				w.Write([]byte(err.Error()))
+			}
+			w.Write(config)
+			return
+		}
+		cookiesEnv := cookies.Value
+		config, err := loadConfig(cookiesEnv)
+		if err != nil {
+			w.Write([]byte("\n \r"))
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write([]byte("\n \r"))
+		w.Write(config)
 	})
+	log.Println("run at :8801")
 	http.ListenAndServe(":8801", nil)
+}
+
+func loadConfig(cookiesEnv string) ([]byte, error) {
+	var path string
+	switch cookiesEnv {
+	case "test":
+		path = "./configs/test.json"
+	case "prod":
+		path = "./configs/prod.json"
+	case "stage":
+		path = "./configs/stage.json"
+	default:
+		path = "./configs/dev.json"
+	}
+	b, err := ioutil.ReadFile(path) // just pass the file name
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
 }
